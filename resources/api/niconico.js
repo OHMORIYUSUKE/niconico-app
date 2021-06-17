8b8dd453359b748a7002e48fac07be9b29531e41
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-06-17 23:59:47 +0900

    headers

--- resources/api/niconico.js
+++ resources/api/niconico.js
@@ -10,6 +10,11 @@ exports.handler = async (event, context) => {
   console.log(JSON.stringify(res.data))
   
   return {
+    headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
     statusCode: 200,
     body: JSON.stringify(res.data),
   };
