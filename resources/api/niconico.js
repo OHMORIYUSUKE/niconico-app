ae9809d67cf18db82f41d1fafb17b50b35b8b3f3
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-06-18 00:31:17 +0900

    Fix number

--- resources/api/niconico.js
+++ resources/api/niconico.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.handler = async (event, context) => {
   const keyWord = event.queryStringParameters.q || ".";
-  const URL = `https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${encodeURIComponent(keyWord)}&targets=title&fields=contentId,title,viewCounter&filters[viewCounter][gte]=10000&_sort=-viewCounter&_offset=0&_limit=18&_context=apiguide`;
+  const URL = `https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${encodeURIComponent(keyWord)}&targets=title&fields=contentId,title,viewCounter&filters[viewCounter][gte]=10000&_sort=-viewCounter&_offset=0&_limit=21&_context=apiguide`;
   //const URL = '';
 
   const res = await axios.get(URL)
